fde6c0c76771de704f328d713b64136ecd10694f
Ian Deane <iandeane@Ians-Mac-mini.local>
2016-02-07 20:00:37 -0800

    Added native bullet builds for iOS and OSX. Removed a lot of C# wrapper code that didn't have corresponding native code (linker errors). Modified MotionState wrappers to use [MonoPInvokeCallback] attribute (necessary for iOS.

---
+++ libbulletc-iOS/libbulletc/libbulletc/libbulletc.m
@@ -0,0 +1,13 @@
+//
+//  libbulletc.m
+//  libbulletc
+//
+//  Created by Ian Deane on 2016-02-05.
+//  Copyright (c) 2016 Ian Deane. All rights reserved.
+//
+
+#import "libbulletc.h"
+
+@implementation libbulletc
+
+@end
---
+++ libbulletc-iOS/libbulletc/libbulletcSrc/btMultiSphereShape_wrap.cpp
@@ -0,0 +1,36 @@
+#include <BulletCollision/CollisionShapes/btMultiSphereShape.h>
+
+#include "conversion.h"
+#include "btMultiSphereShape_wrap.h"
+
+btMultiSphereShape* btMultiSphereShape_new(const btScalar* positions, const btScalar* radi, int numSpheres)
+{
+	btVector3* positionsTemp = new btVector3[numSpheres];
+	for (int i = 0; i < numSpheres; i++)
+	{
+		Vector3TobtVector3(&positions[i*3], &positionsTemp[i]);
+	}
+	btMultiSphereShape* shape = new btMultiSphereShape(positionsTemp, radi, numSpheres);
+	delete[] positionsTemp;
+	return shape;
+}
+
+btMultiSphereShape* btMultiSphereShape_new2(const btVector3* positions, const btScalar* radi, int numSpheres)
+{
+	return new btMultiSphereShape(positions, radi, numSpheres);
+}
+
+int btMultiSphereShape_getSphereCount(btMultiSphereShape* obj)
+{
+	return obj->getSphereCount();
+}
+
+void btMultiSphereShape_getSpherePosition(btMultiSphereShape* obj, int index, btScalar* value)
+{
+	VECTOR3_OUT(&obj->getSpherePosition(index), value);
+}
+
+btScalar btMultiSphereShape_getSphereRadius(btMultiSphereShape* obj, int index)
+{
+	return obj->getSphereRadius(index);
+}
